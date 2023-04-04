9be185cc517110c8713376b62e4f911a11a1d165
Aditya Kumar Sharma <adityakr1001@gmail.com>
2023-04-04 09:59:56 +0530

    portfolio

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -17,7 +17,7 @@ const NavigationDots = (active) => {
             key={item + index}
             className="app__navigation-dot"
             style={active === item ? { backgroundColor: "#313BAC !important" } : {}}
-          />
+          > </a>
         )
       )}
     </div>
